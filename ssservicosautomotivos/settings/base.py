00290eb26d1b307f102aa65f986c51859de3eb80
Davi Silva <mangaestranha@localhost.localdomain>
2019-03-30 02:57:07 -0300

    Website being ready for deployment.

--- ssservicosautomotivos/settings/base.py
+++ ssservicosautomotivos/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,3 +130,14 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
 ]
+
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
